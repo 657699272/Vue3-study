1f79a25c1b926856ddc116cf857f7dfdb79e8f84
shenbinghui <657699272@qq.com>
2023-02-02 21:14:52 +0800

    项目初始化

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: ['plugin:vue/vue3-essential', '@vue/standard', 'plugin:prettier/recommended'],
+  plugins: ['vue'],
+  rules: {},
+};
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    {{ name }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const name = ref('shen');
+</script>
